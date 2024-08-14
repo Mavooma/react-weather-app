474034753f9918dd816b56748c5393d371fb8386
Vuyelwa Mavuma <mavumavuyelwa@gmail.com>
2024-08-14 15:06:35 +0200

    Added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import './App.css';
 
 export default function App() {
-  return 
-    <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>
+        Weather App
+      </h1> 
+      <footer>
+        This project is coded by <a href="https://github.com/Mavooma" target="_blank" rel="noopener noreferrer">Vuyelwa Mavuma</a> and is {" "}
+        <a href="https://github.com/Mavooma/react-weather-app" target="_blank" rel="noopener noreferrer">
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
-
-export default App;
